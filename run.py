5036049d2389bb78de73dd9740fed1524bce839a
dmitriy <dtsamalaidze@mail.ru>
2022-08-08 22:25:41 +0300

    commit 8

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from exceptions.exceptions import BaseAppExceptions
 
 app = Flask(__name__)
 
+
 @app.errorhandler(BaseAppExceptions)
 def page_not_found(e: BaseAppExceptions):
     return render_template(f'{e.code}.html'), e.code
@@ -18,8 +19,5 @@ app.register_blueprint(post_app, url_prefix='/post')
 app.register_blueprint(search_app, url_prefix='/search')
 app.register_blueprint(user_feed_app, url_prefix='/user_feed')
 
-
-
 if __name__ == '__main__':
     app.run(host='localhost', debug=True)
-
